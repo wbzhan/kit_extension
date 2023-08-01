5976c632919ffa63123a1f51b67bef49ddf6f6d3
包子 <wenbao.zhan@urbanic.com>
2023-08-01 16:20:38 +0800

    ▶︎改动所属类型(type):fix ▶︎改动所属模块(scope): ▶︎改动具体描述(subject):

--- kit_extension/Classes/FontExtension.swift
+++ kit_extension/Classes/FontExtension.swift
@@ -8,18 +8,22 @@
 
 import Foundation
 import UIKit
+public var KitFontNameRegular = "Futura-Book"
+public var KitFontNameMedium = "Futura-Medium"
+public var KitFontNameBold = "Futura-Medium"
 
 @objc public extension UIFont {
     //regular
     @objc class func font(_ size: CGFloat) -> UIFont {
-        return UIFont.init(name: "Futura-Book", size: size) ?? UIFont.systemFont(ofSize: size)
+        return UIFont.init(name: KitFontNameRegular, size: size) ?? UIFont.systemFont(ofSize: size)
     }
     //medium
     @objc class func mediumFont(_ size:CGFloat ) ->UIFont {
-        return UIFont.init(name: "Futura-Medium", size: size) ?? UIFont.boldSystemFont(ofSize: size)
+        return UIFont.init(name: KitFontNameMedium, size: size) ?? UIFont.boldSystemFont(ofSize: size)
     }
     //semibold
     @objc class func semiboldFont(_ size: CGFloat) -> UIFont {
-        return UIFont.init(name: "Futura-Medium", size: size) ?? UIFont.boldSystemFont(ofSize: size)
+        return UIFont.init(name: KitFontNameBold, size: size) ?? UIFont.boldSystemFont(ofSize: size)
     }
 }
+
